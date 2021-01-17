dc6998a1b96e0a7fc306ef81bbb3917f80262581
Vahid Abolghasemi <vahid1980ir@gmail.com>
2021-01-17 17:59:44 +0330

    edit price font size

--- app/articles/article/article.jsx
+++ app/articles/article/article.jsx
@@ -3,6 +3,6 @@ export const Article = (props) => {
     return <div className={style.article}>
         <img src={props.article.images[0].url} alt="" />
         <h3>{props.article.title}</h3>
-        <h4>Price:{props.article.price}$</h4>
+        <h2>Price:{props.article.price}$</h2>
     </div>
 }
